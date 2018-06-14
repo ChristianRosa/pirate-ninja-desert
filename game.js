80103148b9d5318bf3a5995472ab34b274bb3961
Christian Rosa <crosa93921@gmail.com>
2018-06-13 18:44:55 -0700

    Level and actors finished

--- game.js
+++ game.js
@@ -27,11 +27,93 @@ class Level {
 				let type = levelTiles[tile];
 				if(typeof type == "string") return type;
 				this.startActors.push(
-					type.create(new Vec(x,y)ss
+					type.create(new Vec(x,y),tile));
+				return "empty";
 			});
 		});
 	}
 }
+
+////Actor Classes
+
+//Player
+class Player {
+	constructor(pos, speed){
+		this.pos = pos;
+		this.speed = speed;
+	}
+
+	get type() {return "player";}
+
+	static create(pos){
+		return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0, 0));
+	}
+}
+
+Player.prototype.size = new Vec(0.8, 1.5);
+
+//Lava
+class Lava {
+	constructor(pos,speed,reset) {
+		this.pos = pos;
+		this.speed = speed;
+		this.reset = reset;
+	}
+
+	get type() {return "lava"}
+
+	static create(pos,tile){
+		if(tile == "="){
+			return new Lava(pos,new Vec(2,0));
+		} else if (tile == "|"){
+			return new Lava(pos, new Vec(0,2));
+		} else if (tile == "v"){
+			return new Lava(pos, new Vec(0,3), pos);
+		}
+	}
+}
+
+Lava.prototype.size = new Vec(1,1);
+
+//Coin
+class Coin {
+	constructor(pos,basePos,wobble){
+		this.pos = pos;
+		this.basePos = basePos;
+		this.wobble = wobble;
+	}
+
+	get type() {return "coin";}
+
+	static create(pos){
+		let basePos = pos.plus(new Vec(0.2,0.1));
+		return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
+	}
+}
+
+
+Coin.prototype.size = new Vec(0.6 ,0.6);
+
+
+//State management
+
+class State {
+	constructor(level, actors, status){
+		this.level = level;
+		this.actors = actors;
+		this.status = status;
+	}
+
+	static start(level){
+		return new State(level, level.startActors,"playing");
+	}
+
+	get player(){
+		return this.actors.find(a => a.type == "player")
+	}
+}
+
+//Vector Class
 
 class Vec{
 	constructor(x,y){
