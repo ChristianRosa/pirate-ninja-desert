e8b4ff3665a6b84bebe552868665f460a9062380
Christian Rosa <crosa93921@gmail.com>
2018-06-17 18:31:20 -0700

    Added viewport handling for movement

--- display.js
+++ display.js
@@ -1,3 +1,5 @@
+////Create and Display elements
+//Create
 function createElm (name,atts,...children){
 	let dom = document.createElement(name);
 	for (let att of Object.keys(atts)) {
@@ -9,6 +11,7 @@ function createElm (name,atts,...children){
 	return dom;
 }
 
+//Display
 class DOMDisplay {
 	constructor(parent,level){
 		this.dom = createElm("div",{class:"game"},drawGrid(level));
@@ -19,6 +22,15 @@ class DOMDisplay {
 	clear() {this.dom.remove();}
 
 }
+
+//Enables display to show different states. Redraws actors as needed for movement
+DOMDisplay.prototype.syncState = function(state){
+	if(this.actorLayer) this.actorLayer.remove();
+	this.actorLayer = drawActors(state.actors);
+	this.dom.appendChild(this.actorLayer);
+	this.dom.className = `game ${state.status}`;
+	this.scrollPlayerIntoView(state);
+};
 
 const scale = 20;
 
@@ -31,3 +43,38 @@ function drawGrid(level) {
 			...row.map(type => createElm("td", {class:type}))
 		)));
 }
+
+function drawActors(actors){
+	return createElm("div",{}, ...actors.map(actor => {
+		let rect = createElm("div", {class:`actor ${actor.type}`});
+		rect.style.width = `${actor.size.x * scale}px`;
+		rect.style.height = `${actor.size.y * scale}px`;
+		rect.style.left = `${actor.pos.x * scale}px`;
+		rect.style.top = `${actor.pos.y * scale}px`;
+		return rect;
+	}));
+}
+
+//Makes sure player is always in view when moving
+DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
+	let width = this.dom.clientWidth;
+	let height = this.dom.clientHeight;
+	let margin = width/3;
+
+	let left = this.dom.scrollLeft, right = left + width;
+	let top = this.dom.scrollTop, bottom = top + height;
+
+	let player = state.player;
+	let center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+	if(center.x < left + margin) {
+		this.dom.scrollLeft = center.x - margin;
+	} else if (center.x > right - margin) {
+		this.dom.scrollLeft = center.x - margin - width;
+	}
+	if (center.y < top + margin) {
+		this.dom.scrollTop = center.y - margin;
+	} else if (center.y > bottom - margin) {
+		this.dom.scrollTop = center.y + margin - height;
+	}
+};
